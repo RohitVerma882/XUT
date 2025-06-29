2b82eccfde08188eda5be05d18f2da0765fadd5a
RohitVerma882 <rohitkumar882333@gmail.com>
2025-06-29 16:29:35 +0530

    cleanup

--- app/src/main/java/com/xut/Constants.kt
+++ app/src/main/java/com/xut/Constants.kt
@@ -1,7 +1,7 @@
 package com.xut
 
 object Constants {
-    const val GITHUB_SOURCE_CODE_URL = "https://github.com/RohitVerma882/XUnlockTool"
+    const val GITHUB_SOURCE_CODE_URL = "https://github.com/RohitVerma882/XUT"
 
     const val LOGIN_URL =
         "https://account.xiaomi.com/pass/serviceLogin?sid=unlockApi&json=false&passive=true&hidden=false&_snsDefault=facebook&checkSafePhone=true&_locale=en"
